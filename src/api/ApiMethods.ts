f28d520e2e9fbac9960b8ebf4306f707a6973d08
Serkan Kaan Kuheylan <serkankaankuheylan@gmail.com>
2023-11-24 11:24:18 +0300

    baseUrl

--- src/api/ApiMethods.ts
+++ src/api/ApiMethods.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import store from "../store";
 import { clearUser } from "../store/slices/userSlice";
 
-const baseURL = "http://localhost:3001";
+const baseURL = "https://summaryai.io";
 
 // header
 const headers = {
